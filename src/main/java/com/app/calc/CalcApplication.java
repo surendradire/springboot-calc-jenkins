86967d7ec282697e16b69c0b33b790655ab5e0a6
Vinayak Bansal <Vinayak.Bansal@watchguard.com>
2024-10-22 11:42:15 +0530

    Changes done

--- src/main/java/com/app/calc/CalcApplication.java
+++ src/main/java/com/app/calc/CalcApplication.java
@@ -2,11 +2,19 @@ package com.app.calc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class CalcApplication {
+public class CalcApplication extends SpringBootServletInitializer {
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CalcApplication.class);
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(CalcApplication.class, args);
     }
 }
+
--- src/main/java/com/app/calc/controller/CalcController.java
+++ src/main/java/com/app/calc/controller/CalcController.java
@@ -1,14 +1,16 @@
 package com.app.calc.controller;
-import com.app.calc.service.CalcService;
 
+import com.app.calc.service.CalcService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
+//@RequestMapping("/calc")
 public class CalcController {
 
     @Autowired
